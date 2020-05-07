44b332433d77b8bcc3d5276cd5725af3b310d2a7
Arif <arif.cse.baust@gmail.com>
2020-05-07 21:55:53 +0600

    Update excercise.py
    Solution of the excercise

--- excercise.py
+++ excercise.py
@@ -1,8 +1,10 @@
 # !/usr/bin/env python
-my_list=[1,23,45,'hello','43',-23,'-56','one',5.6]
+my_list=[1,23,45,'hello','43',-23,'-56','one',2.0]
 anotherList=[]
 for i in my_list:
     try:
+        if type(i)==float and  str:
+            raise Exception()
         if i!=float(i):
             i=int(i)
         if i==int(i):
